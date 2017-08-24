30100ee6cc81197961f5975880c3ee4d949ecafe
Sujit singh <sujit.singh@outlook.com>
2017-08-24 17:36:15 +0100

    version updated to fix typo

--- ILR Learner Entry 1718/ilrLearnerEntry/Properties/AssemblyInfo.cs
+++ ILR Learner Entry 1718/ilrLearnerEntry/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1718.2.0.*")]
-[assembly: AssemblyFileVersion("17182.0")]
+[assembly: AssemblyFileVersion("1718.2.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
