a1d6471adefdb47feff3f1456cbb8caadd066a85
Sujit singh <sujit.singh@outlook.com>
2017-09-08 15:41:27 +0100

    new version for demo

--- ILR Learner Entry 1718/ilrLearnerEntry/Properties/AssemblyInfo.cs
+++ ILR Learner Entry 1718/ilrLearnerEntry/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1718.2.0.*")]
-[assembly: AssemblyFileVersion("1718.0.2")]
+[assembly: AssemblyFileVersion("1718.0.3")]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
