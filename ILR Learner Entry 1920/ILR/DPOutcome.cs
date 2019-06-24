064d3eb9c392d6edea8a7ee03b584f1479630e87
Gary Griffiths <Gary.Griffiths@amorgroup.com>
2019-06-24 09:23:06 +0100

    Formatting

--- ILR Learner Entry 1920/ILR/DPOutcome.cs
+++ ILR Learner Entry 1920/ILR/DPOutcome.cs
@@ -12,19 +12,83 @@ namespace ILR
     {
 
         #region ILR Properties
-        public string OutType { get { return XMLHelper.GetChildValue("OutType", Node, NSMgr); } set { XMLHelper.SetChildValue("OutType", value, Node, NSMgr); PropertyChanged("OutType"); } }
-        public int? OutCode { get { string OutCode = XMLHelper.GetChildValue("OutCode", Node, NSMgr); return (!string.IsNullOrEmpty(OutCode) ? int.Parse(OutCode) : (int?)null); } set { XMLHelper.SetChildValue("OutCode", value, Node, NSMgr); PropertyChanged("OutCode"); } }
-        public DateTime? OutStartDate { get { string OutStartDate = XMLHelper.GetChildValue("OutStartDate", Node, NSMgr); return (!string.IsNullOrEmpty(OutStartDate) ? DateTime.Parse(OutStartDate) : (DateTime?)null); } set { XMLHelper.SetChildValue("OutStartDate", value, Node, NSMgr); PropertyChanged("OutStartDate"); } }
-        public DateTime? OutEndDate { get { string OutEndDate = XMLHelper.GetChildValue("OutEndDate", Node, NSMgr); return (!string.IsNullOrEmpty(OutEndDate) ? DateTime.Parse(OutEndDate) : (DateTime?)null); } set { XMLHelper.SetChildValue("OutEndDate", value, Node, NSMgr); PropertyChanged("OutEndDate"); } }
-        public DateTime? OutCollDate { get { string OutCollDate = XMLHelper.GetChildValue("OutCollDate", Node, NSMgr); return (!string.IsNullOrEmpty(OutCollDate) ? DateTime.Parse(OutCollDate) : (DateTime?)null); } set { XMLHelper.SetChildValue("OutCollDate", value, Node, NSMgr); PropertyChanged("OutCollDate"); } }
+
+        public string OutType
+        {
+            get { return XMLHelper.GetChildValue("OutType", Node, NSMgr); }
+            set
+            {
+                XMLHelper.SetChildValue("OutType", value, Node, NSMgr);
+                PropertyChanged("OutType");
+            }
+        }
+
+        public int? OutCode
+        {
+            get
+            {
+                string OutCode = XMLHelper.GetChildValue("OutCode", Node, NSMgr);
+                return (!string.IsNullOrEmpty(OutCode) ? int.Parse(OutCode) : (int?) null);
+            }
+            set
+            {
+                XMLHelper.SetChildValue("OutCode", value, Node, NSMgr);
+                PropertyChanged("OutCode");
+            }
+        }
+
+        public DateTime? OutStartDate
+        {
+            get
+            {
+                string OutStartDate = XMLHelper.GetChildValue("OutStartDate", Node, NSMgr);
+                return (!string.IsNullOrEmpty(OutStartDate) ? DateTime.Parse(OutStartDate) : (DateTime?) null);
+            }
+            set
+            {
+                XMLHelper.SetChildValue("OutStartDate", value, Node, NSMgr);
+                PropertyChanged("OutStartDate");
+            }
+        }
+
+        public DateTime? OutEndDate
+        {
+            get
+            {
+                string OutEndDate = XMLHelper.GetChildValue("OutEndDate", Node, NSMgr);
+                return (!string.IsNullOrEmpty(OutEndDate) ? DateTime.Parse(OutEndDate) : (DateTime?) null);
+            }
+            set
+            {
+                XMLHelper.SetChildValue("OutEndDate", value, Node, NSMgr);
+                PropertyChanged("OutEndDate");
+            }
+        }
+
+        public DateTime? OutCollDate
+        {
+            get
+            {
+                string OutCollDate = XMLHelper.GetChildValue("OutCollDate", Node, NSMgr);
+                return (!string.IsNullOrEmpty(OutCollDate) ? DateTime.Parse(OutCollDate) : (DateTime?) null);
+            }
+            set
+            {
+                XMLHelper.SetChildValue("OutCollDate", value, Node, NSMgr);
+                PropertyChanged("OutCollDate");
+            }
+        }
+
         #endregion
 
         #region Constructors
+
         internal DPOutcome(XmlNode DPOutcomeNode, XmlNamespaceManager NSMgr)
         {
             this.Node = DPOutcomeNode;
             this.NSMgr = NSMgr;
         }
+
         internal DPOutcome(DPOutcome MigrationDPOutcome, XmlNode Node, XmlNamespaceManager NSMgr)
         {
             this.Node = Node;
@@ -37,6 +101,7 @@ namespace ILR
             this.OutCollDate = MigrationDPOutcome.OutCollDate;
 
         }
+
         #endregion
 
         public event PropertyChangedEventHandler OutcomeChanged;
@@ -46,7 +111,9 @@ namespace ILR
             if (OutcomeChanged != null)
                 OutcomeChanged(this, new PropertyChangedEventArgs("From LD record"));
         }
+
         #region overrided methods
+
         public override bool IsComplete
         {
             get
@@ -70,6 +137,7 @@ namespace ILR
                 return message;
             }
         }
+
         #endregion
 
         #region IDataErrorInfo Members
@@ -84,16 +152,17 @@ namespace ILR
             get
             {
                 string result = null;
-                if (columnName == "OutType")
-                {
-                    if (String.IsNullOrEmpty(OutType))
-                        result = String.Format("{0} required.\r\n", columnName);
+                //if (columnName == "OutType")
+                //{
+                    //if (String.IsNullOrEmpty(OutType))
+                        //result = String.Format("{0} required.\r\n", columnName);
 
                     //if (OutType == null)
                     //{
                     //    result = "OutType not supplied.";
                     //}
-                }
+                //}
+
                 if (columnName == "OutCode")
                 {
                     if (OutCode != null && OutCode.ToString().Length > 8)
@@ -102,11 +171,13 @@ namespace ILR
                         //OutCode = (int?)int.Parse(OutCode.ToString().Substring(0, 8));
                     }
                 }
+
                 return result;
             }
         }
 
         #endregion
+
         public void PropertyChanged(string propertyName)
         {
             OnPropertyChanged(propertyName);
